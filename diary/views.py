db76e45d1088148230675511c1aeba5a2b634711
YutaroTakei <yutarotakei@icloud.com>
2020-12-10 21:42:46 +0900

    third

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib import messages
+
+import logging
+from django.urls import reverse_lazy
+
+from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = 'inquiry.html'
+    form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
---
+++ private_diary/settings_dev.py
@@ -0,0 +1,43 @@
+from .setting_common import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'loggers':{
+        'django':{
+            'handlers':['console'],
+            'level':'INFO',
+        },
+        'diary':{
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+    },
+    'handlers':{
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter':'dev'
+        },
+    },
+
+    'formatters':{
+        'dev':{
+            'format':'\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(messages)s'
+            ])
+        },
+    }
+
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
