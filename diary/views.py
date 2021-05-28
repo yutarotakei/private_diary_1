168194bfd177945e5bfb81eae380f294c98cf64b
YutaroTakei <yutarotakei@icloud.com>
2021-05-28 22:11:52 +0900

    update

--- diary/views.py
+++ diary/views.py
@@ -36,7 +36,7 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 2
 
     def get_queryset(self):
-        diaries = Diary.objects.filter(user=self.request.user).orderby('-created_at')
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
 
 
